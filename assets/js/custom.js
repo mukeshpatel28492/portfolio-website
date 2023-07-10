d90f590ee8d7be7466d60f2fe94656dbcf8a2334
Mukesh Patel <mukeshpatel@Apples-MacBook-Pro-9.local>
2023-07-10 19:37:34 +0530

    about-us section

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,20 +1,9 @@
 (function ($) {
+
+
     /* Document Ready State */
     jQuery(document).ready(function(){
-        //jQuery('body').css('background','#fff');
-        // window.addEventListener('scroll', function() {
-        //     var header = document.querySelector('.header-section');
-        //     var scrollPosition = window.scrollY;
-          
-        //     if (scrollPosition > 0) {
-        //       header.classList.add('scrolled');
-        //     } else {
-        //       header.classList.remove('scrolled');
-        //     }
-        //   });
-
-
-
+        AOS.init(); 
           /*===== Header sticky =====*/
 	$(window).scroll(function () {
 		var scroll = $(window).scrollTop();
@@ -26,7 +15,7 @@
 	});
 	/*===== Header sticky =====*/
 
-
+   
 
     });
 })(jQuery);
