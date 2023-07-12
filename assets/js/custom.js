259506df252a155fc71733a1b357a16c8bd3feb8
Mukesh Patel <mukeshpatel@Apples-MacBook-Pro-9.local>
2023-07-12 19:04:16 +0530

    services-section

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -15,7 +15,23 @@
 	});
 	/*===== Header sticky =====*/
 
-   
+    /*===== Testimonial slider =====*/
+
+    $(document).ready(function(){
+        $('.center-slider').slick({
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          centerMode: true,
+          arrows: true,
+          dots: false,
+          speed: 300,
+          centerPadding: '20px',
+          infinite: true,
+          autoplaySpeed: 5000,
+          autoplay: true
+        });
+      });
+      /*===== Testimonial slider =====*/
 
     });
 })(jQuery);
